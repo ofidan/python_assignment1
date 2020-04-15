466c31941cb002b029b267ce126b2fd177d251f1
Oğuzhan Fidan <ofidan5435@gmail.com>
2020-04-16 00:15:03 +0300

    python assignment1

---
+++ Untitled5.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Control flow statements
+
+# In[1]:
+
+
+name = input("enter your first name: ")
+
+if name == "Oliver":
+    print("Hello, Oliver! The password is : @12")
+else:
+    print("Hello, {}! See you later.".format(name))
+
+
+# In[ ]:
+
+
+
+
